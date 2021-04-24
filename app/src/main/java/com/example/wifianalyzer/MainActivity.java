435aecce241724137b9207ceb97c286a3b227c7d
yashdavisgupta <yashdavisgupta@gmail.com>
2021-04-24 14:16:54 -0400

    Add fine location permission request

--- app/src/main/java/com/example/wifianalyzer/MainActivity.java
+++ app/src/main/java/com/example/wifianalyzer/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.wifianalyzer;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
     @Override
      protected void onCreate(Bundle savedInstanceState){
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                1);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
      }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    Toast.makeText(MainActivity.this, "Location permission is required for AP Scanning", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
 }
